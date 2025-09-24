25cf447c50490552d0afe59ddc57b47ae19df039
K. V. Diya Sahasra <kvdiya06@gmail.com>
2025-09-24 18:33:44 +0530

    Update password_generator_2.py

--- password_generator_2.py
+++ password_generator_2.py
@@ -11,15 +11,15 @@ letter_needed_1=[]
 #symbols_needed_1=[]
 for i in range(letter_needed):
     n=len(letter)-1
-    letter_index=random.randint(1,n)
+    letter_index=random.randint(0,n)
     letter_needed_1.append(letter[letter_index])
 for i in range(numbers_needed):    
-    numbers_index=random.randint(1,(len(numbers)-1))
+    numbers_index=random.randint(0,(len(numbers)-1))
     letter_needed_1.append(numbers[numbers_index])
 for i in range(symbols_needed):  
-    symbols_index=random.randint(1,(len(symbols)-1))
+    symbols_index=random.randint(0,(len(symbols)-1))
     letter_needed_1.append(symbols[symbols_index])
 for i in range(len(letter_needed_1)):
-    etter_index=random.randint(1,(len(letter_needed_1)-1))
+    etter_index=random.randint(0,(len(letter_needed_1)-1))
     #v=
     print(letter_needed_1[etter_index],end="")
